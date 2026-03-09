3a1117f314d20a558b943c6c64bec5bd956bb408
RadyaI <radyaiftikhar@gmail.com>
2026-03-09 17:25:55 +0700

    Tambah provider di types user

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,10 +1,13 @@
 export type UserRole = "mahasiswa" | "admin"
+export type AuthProvider = "google" | "password"
 
 export type User = {
   userId: string
   name: string
   email: string
   phoneNumber?: string
+  avatar?: string
+  provider: AuthProvider
   role: UserRole
   createdAt: Date
 }
